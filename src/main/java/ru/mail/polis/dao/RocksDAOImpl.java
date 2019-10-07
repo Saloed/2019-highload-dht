076fb5a1e6a3c9e95e2c32486c5f66e10df7fa5c
Saloed <v.sobol@kspt.icc.spbstu.ru>
2019-10-07 20:54:33 +0300

    fix more codeclimate issues

--- src/main/java/ru/mail/polis/dao/RocksDAOImpl.java
+++ src/main/java/ru/mail/polis/dao/RocksDAOImpl.java
@@ -14,7 +14,7 @@ import java.util.NoSuchElementException;
 public class RocksDAOImpl implements DAO {
     private final RocksDB db;
 
-    private RocksDAOImpl(RocksDB db) {
+    private RocksDAOImpl(final RocksDB db) {
         this.db = db;
     }
 
