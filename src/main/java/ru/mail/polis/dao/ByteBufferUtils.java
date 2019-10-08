daff0f6454fe4a43c2213ed5fe3db56866d3c774
Saloed <v.sobol@kspt.icc.spbstu.ru>
2019-10-08 13:09:44 +0300

    fix RocksDB comparator

--- src/main/java/ru/mail/polis/dao/ByteBufferUtils.java
+++ src/main/java/ru/mail/polis/dao/ByteBufferUtils.java
@@ -3,6 +3,7 @@ package ru.mail.polis.dao;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public final class ByteBufferUtils {
 
@@ -17,6 +18,64 @@ public final class ByteBufferUtils {
         final var array = new byte[bufferCopy.remaining()];
         bufferCopy.get(array);
         return array;
+    }
+
+
+    /**
+     * Wraps array into {@link java.nio.ByteBuffer}
+     *
+     * @param array -- byte array to wrap
+     * @return ByteBuffer
+     */
+    public static ByteBuffer fromArray(@NotNull final byte[] array){
+        return ByteBuffer.wrap(array);
+    }
+
+    /**
+     * Retrieve array from a {@link java.nio.ByteBuffer} and shift all bytes by {@link Byte#MIN_VALUE}
+     * This hack fix the issue with {@link org.rocksdb.BuiltinComparator#BYTEWISE_COMPARATOR}
+     * https://github.com/facebook/rocksdb/issues/5891
+     *
+     * @param buffer -- byte buffer to extract from
+     * @return array with all bytes shifted
+     */
+
+    public static byte[] toArrayShifted(@NotNull final ByteBuffer buffer) {
+        final var bufferCopy = buffer.duplicate();
+        final var array = new byte[bufferCopy.remaining()];
+        bufferCopy.get(array);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = toUnsignedByte(array[i]);
+        }
+        return array;
+    }
+
+    /**
+     * Wrap byte array into {@link java.nio.ByteBuffer}
+     * See {@link ByteBufferUtils#toArrayShifted} for details about shift
+     *
+     * @param array -- byte array to wrap
+     * @return ByteBuffer with all bytes shifted back to normal values
+     */
+    public static ByteBuffer fromArrayShifted(@NotNull final byte[] array) {
+        final var arrayCopy = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < arrayCopy.length; i++) {
+            arrayCopy[i] = fromUnsignedByte(arrayCopy[i]);
+        }
+        return ByteBuffer.wrap(arrayCopy);
+    }
+
+
+    private static byte toUnsignedByte(final byte b) {
+        final var uint = Byte.toUnsignedInt(b);
+        final var shiftedUint = uint - Byte.MIN_VALUE;
+        return (byte) shiftedUint;
+    }
+
+    private static byte fromUnsignedByte(final byte b) {
+        final var uint = Byte.toUnsignedInt(b);
+        final var shiftedUint = uint + Byte.MIN_VALUE;
+        return (byte) shiftedUint;
     }
 
 }
--- src/main/java/ru/mail/polis/dao/RocksDAOImpl.java
+++ src/main/java/ru/mail/polis/dao/RocksDAOImpl.java
@@ -1,8 +1,13 @@
 package ru.mail.polis.dao;
 
 import org.jetbrains.annotations.NotNull;
-import org.rocksdb.*;
-import org.rocksdb.util.BytewiseComparator;
+
+import org.rocksdb.BuiltinComparator;
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+import org.rocksdb.RocksIterator;
+
 import ru.mail.polis.Record;
 
 import java.io.File;
@@ -18,44 +23,23 @@ public class RocksDAOImpl implements DAO {
         this.db = db;
     }
 
-
-    public static class RocksRecordIterator implements Iterator<Record>, AutoCloseable {
-
-        private final RocksIterator iterator;
-
-        RocksRecordIterator(@NotNull final RocksIterator iterator) {
-            this.iterator = iterator;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return iterator.isValid();
-        }
-
-        @Override
-        public Record next() {
-            if (!hasNext()) {
-                throw new IllegalStateException("Iterator is exhausted");
-            }
-            final var keyByteArray = iterator.key();
-            final var valueByteArray = iterator.value();
-            final var key = ByteBuffer.wrap(keyByteArray);
-            final var value = ByteBuffer.wrap(valueByteArray);
-            final var record = Record.of(key, value);
-            iterator.next();
-            return record;
-        }
-
-        @Override
-        public void close() throws Exception {
-            iterator.close();
+    static DAO create(File data) throws IOException {
+        RocksDB.loadLibrary();
+        try {
+            final var options = new Options()
+                    .setCreateIfMissing(true)
+                    .setComparator(BuiltinComparator.BYTEWISE_COMPARATOR);
+            final var db = RocksDB.open(options, data.getAbsolutePath());
+            return new RocksDAOImpl(db);
+        } catch (RocksDBException exception) {
+            throw new DAOException("Can't obtain db instance", exception);
         }
     }
 
     @NotNull
     @Override
     public Iterator<Record> iterator(@NotNull ByteBuffer from) throws IOException {
-        final var fromByteArray = ByteBufferUtils.toArray(from);
+        final var fromByteArray = ByteBufferUtils.toArrayShifted(from);
         final var iterator = db.newIterator();
         iterator.seek(fromByteArray);
         return new RocksRecordIterator(iterator);
@@ -64,13 +48,13 @@ public class RocksDAOImpl implements DAO {
     @NotNull
     @Override
     public ByteBuffer get(@NotNull ByteBuffer key) throws IOException, NoSuchElementException {
-        final var keyByteArray = ByteBufferUtils.toArray(key);
+        final var keyByteArray = ByteBufferUtils.toArrayShifted(key);
         try {
             final var valueByteArray = db.get(keyByteArray);
             if (valueByteArray == null) {
                 throw new NoSuchElementExceptionLite("Key is not present: " + key.toString());
             }
-            return ByteBuffer.wrap(valueByteArray);
+            return ByteBufferUtils.fromArray(valueByteArray);
         } catch (RocksDBException exception) {
             throw new DAOException("Error while get", exception);
         }
@@ -78,7 +62,7 @@ public class RocksDAOImpl implements DAO {
 
     @Override
     public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) throws IOException {
-        final var keyByteArray = ByteBufferUtils.toArray(key);
+        final var keyByteArray = ByteBufferUtils.toArrayShifted(key);
         final var valueByteArray = ByteBufferUtils.toArray(value);
         try {
             db.put(keyByteArray, valueByteArray);
@@ -89,7 +73,7 @@ public class RocksDAOImpl implements DAO {
 
     @Override
     public void remove(@NotNull ByteBuffer key) throws IOException {
-        final var keyByteArray = ByteBufferUtils.toArray(key);
+        final var keyByteArray = ByteBufferUtils.toArrayShifted(key);
         try {
             db.delete(keyByteArray);
         } catch (RocksDBException exception) {
@@ -116,23 +100,36 @@ public class RocksDAOImpl implements DAO {
         }
     }
 
-    /**
-     * According to the documentation {@link org.rocksdb.BuiltinComparator#BYTEWISE_COMPARATOR} is a
-     * native implementation of  {@link org.rocksdb.util.BytewiseComparator} which works a way faster.
-     * But for some unknown reasons, iteration order is different and appropriate tests fails
-     */
-    static DAO create(File data) throws IOException {
-        RocksDB.loadLibrary();
-        try {
-            final var comparator = new BytewiseComparator(new ComparatorOptions());
-            final var options = new Options()
-                    .setCreateIfMissing(true)
-                    .setComparator(comparator);
-//                  .setComparator(BuiltinComparator.BYTEWISE_COMPARATOR);
-            final var db = RocksDB.open(options, data.getAbsolutePath());
-            return new RocksDAOImpl(db);
-        } catch (RocksDBException exception) {
-            throw new DAOException("Can't obtain db instance", exception);
+    public static class RocksRecordIterator implements Iterator<Record>, AutoCloseable {
+
+        private final RocksIterator iterator;
+
+        RocksRecordIterator(@NotNull final RocksIterator iterator) {
+            this.iterator = iterator;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.isValid();
+        }
+
+        @Override
+        public Record next() {
+            if (!hasNext()) {
+                throw new IllegalStateException("Iterator is exhausted");
+            }
+            final var keyByteArray = iterator.key();
+            final var valueByteArray = iterator.value();
+            final var key = ByteBufferUtils.fromArrayShifted(keyByteArray);
+            final var value = ByteBufferUtils.fromArray(valueByteArray);
+            final var record = Record.of(key, value);
+            iterator.next();
+            return record;
+        }
+
+        @Override
+        public void close() throws Exception {
+            iterator.close();
         }
     }
 
